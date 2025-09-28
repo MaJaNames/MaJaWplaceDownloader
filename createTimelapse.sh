08a6b015dbe103e72da3344ec343a4e489414138
Fraise <42037799+MaJaNames@users.noreply.github.com>
2025-09-28 15:01:43 +1100

    Update createTimelapse.sh

--- createTimelapse.sh
+++ createTimelapse.sh
@@ -8,13 +8,13 @@ rm -f frames/*.png
 
 i=0
 for h in $(git log --follow --format=%H -- example.png | tac); do
-    git show "$h:./example.png" > "frames/$(printf "%04d" $i).png"
+    git show "$h:./example.png" > "frames/$(printf "image%04d" $i).png"
     i=$((i + 1))
 done
 
-ffmpeg -framerate 6 -pattern_type glob -i 'frames/*.png' \
+ffmpeg -framerate 2 -i 'frames/image%04d.png' \
   -vf "palettegen=reserve_transparent=1" -y palette.png
 
-ffmpeg -framerate 6 -pattern_type glob -i 'frames/*.png' -i palette.png \
+ffmpeg -framerate 2 -i 'frames/image%04d.png' -i palette.png \
   -lavfi "paletteuse=dither=bayer:diff_mode=rectangle" \
   -gifflags -transdiff -y timelapse.gif
